2c09d709b41b5b9f7125f16cef1ae44b99dd4e38
SockHungryClutz <throwaway480085@gmail.com>
2025-06-04 21:31:41 -0700

    Other small functional changes, help, etc.

--- kritamoderngl/kritamoderngl.py
+++ kritamoderngl/kritamoderngl.py
@@ -1,6 +1,7 @@
 from krita import *
 from PyQt5.QtCore import Qt, QRect, QSettings, QStandardPaths
-from PyQt5.QtWidgets import QDialog, QLabel, QDialogButtonBox, QVBoxLayout, QMessageBox
+from PyQt5.QtGui import QIntValidator, QFont
+from PyQt5.QtWidgets import QDialog, QLabel, QHBoxLayout, QVBoxLayout, QMessageBox, QLineEdit, QTextEdit
 from zipfile import ZipFile
 import logging
 import platform
@@ -14,33 +15,45 @@ class MainDialog(QDialog):
         super(MainDialog, self).__init__(parent)
         self.ext = extension
         
-        #self.helpWindow = QMessageBox()
-        # TODO: custom labelled buttons?
-        self.buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Close, self)
+        self.helpWindow = QMessageBox()
+        self.buttonBox = QDialogButtonBox(self)
+        self.buttonBox.addButton("Run", QDialogButtonBox.AcceptRole)
+        self.buttonBox.addButton("Help", QDialogButtonBox.HelpRole)
+        self.buttonBox.addButton("Close", QDialogButtonBox.RejectRole)
         self.setWindowModality(Qt.WindowModal)
         self.buttonBox.accepted.connect(self.applyChanges)
-        # TODO: save window settings even when cancelled
-        self.buttonBox.rejected.connect(self.reject)
-        #self.buttonBox.helpRequested.connect(self.showHelp)
+        self.buttonBox.rejected.connect(self.saveAndReject)
+        self.buttonBox.helpRequested.connect(self.showHelp)
+        monoFont = QFont("Monospace")
+        monoFont.setStyleHint(QFont.TypeWriter)
         
+        self.vertLayout = QHBoxLayout()
         self.vertLabel = QLabel("Vertex Shader:", self)
+        self.vertLabel2 = QLabel("Number of vertices to render:", self)
+        self.vertNumber = QLineEdit("-1", self)
+        self.vertNumber.setValidator(QIntValidator(-1, 2147483647, self))
+        self.vertLayout.addWidget(self.vertLabel2)
+        self.vertLayout.addWidget(self.vertNumber)
         self.vertBox = QTextEdit()
         self.vertBox.setAcceptRichText(False)
         self.vertBox.setTabChangesFocus(False)
+        self.vertBox.setFont(monoFont)
         
         self.fragLabel = QLabel("Fragment Shader:", self)
         self.fragBox = QTextEdit()
         self.fragBox.setAcceptRichText(False)
         self.fragBox.setTabChangesFocus(False)
+        self.fragBox.setFont(monoFont)
         
         self.errLabel = QLabel("Errors:", self)
         self.errBox = QTextEdit()
         self.errBox.setAcceptRichText(False)
         self.errBox.setReadOnly(True)
-        self.errBox.setPlaceholderText("Enter shader code above and click OK, warnings and errors will appear here.")
+        self.errBox.setPlaceholderText("Enter shader code above and click Run, warnings and errors will appear here.")
         
         vbox = QVBoxLayout(self)
         vbox.addWidget(self.vertLabel)
+        vbox.addLayout(self.vertLayout)
         vbox.addWidget(self.vertBox)
         vbox.addWidget(self.fragLabel)
         vbox.addWidget(self.fragBox)
@@ -58,7 +71,7 @@ class MainDialog(QDialog):
     def applyChanges(self):
         doc = Krita.instance().activeDocument()
         if not doc:
-            self.errBox.setPlainText("!ERROR!: You need to have a document open to use this script!")
+            self.errBox.setPlainText("You need to have a document open to use this script!")
             return
         # Get information for the buffer format
         # Number of components is the number of capitals in the color model, unless GRAYA
@@ -85,17 +98,14 @@ class MainDialog(QDialog):
         outFrameBuffer.use() # Through magic, framebuffers are automatically used as output(?)
         # TODO: test if selected layer is ctually being piped in properly and useable
         inputTexture.use()
-        # Hacky workaround, use the length of the first vec3 as the number of vertices
-        # If you need to render more, add a comment like "vec3 vertices[LENGTH]"
-        # TODO: Add a box where the user can put in however many vertices themselves
         vao = self.ext.ctx.vertex_array(program, [])
-        vertShader = self.vertBox.toPlainText()
         try:
-            vertices = int(vertShader[vertShader.find("[",vertShader.find("vec3"))+1:vertShader.find("]",vertShader.find("vec3"))])
+            vertices = int(self.vertNumber.text())
             vao.vertices = vertices
         except ValueError as e:
             # Could not parse number of vertices, good luck
             pass
+        
         self.ext.ctx.clear()
         # Display any errors in warningWidget
         try:
@@ -107,7 +117,7 @@ class MainDialog(QDialog):
             node.parentNode().addChildNode(curNode, doc.activeNode())
             doc.refreshProjection()
         except Exception as e:
-            self.errBox.setPlainText("!ERROR!: " + str(e))
+            self.errBox.setPlainText(str(e))
         # Cleanup
         inputTexture.release()
         outputTexture.release()
@@ -116,6 +126,23 @@ class MainDialog(QDialog):
         program.release()
         self.saveSettings()
 
+    def showHelp(self):
+        self.helpWindow.setText("Krita ModernGL Render Shader Programming")
+        self.helpWindow.setInformativeText("""This tool is designed for running GLSL vertex and fragment shaders inside of Krita and rendering their output to a new layer in the current document. If you would like to learn more, https://learnopengl.com has good tutorials. Here are some more useful bits of info:
+
+   > No vertices are fed into the vertex shader from the program, you will need to define your own vertices inside the shader to render.
+   > Use the text box above the vertex shader to specify how many vertices are to be processed.
+   > The render primitive mode is triangles since this is the default.
+   > Varyings output from the vertex shader can be used as inputs to the fragment shader.
+   > The output of the fragment shader will be rendered to a new layer added above the current selected layer.
+   > The current selected layer can be used as a texture input if you use its texture unit.
+   > There is no syntax highlighting, it is advisable you use some other editor to make the shaders.""")
+        self.helpWindow.exec()
+
+    def saveAndReject(self):
+        self.saveSettings()
+        self.reject()
+
     def closeEvent(self, event):
         self.saveSettings()
         event.accept()
@@ -123,6 +150,7 @@ class MainDialog(QDialog):
     def saveSettings(self):
         rect = self.geometry()
         self.ext.settings.setValue("mgl_geometry", rect)
+        self.ext.settings.setValue("mgl_vert_number", self.vertNumber.text())
         if self.vertBox.toPlainText() != "":
             self.ext.settings.setValue("mgl_vert_shader", self.vertBox.toPlainText())
         if self.fragBox.toPlainText() != "":
@@ -132,6 +160,7 @@ class MainDialog(QDialog):
     def readSettings(self):
         rect = self.ext.settings.value("mgl_geometry", QRect(200, 200, 800, 800))
         self.setGeometry(rect)
+        self.vertNumber.setText(self.ext.settings.value("mgl_vert_number", "-1"))
         self.vertBox.setPlainText(self.ext.settings.value("mgl_vert_shader", ""))
         self.fragBox.setPlainText(self.ext.settings.value("mgl_frag_shader", ""))
 
@@ -202,13 +231,6 @@ class KritaModernGL(Extension):
         pass
 
     def ModernGLWindow(self):
-        # Build the window here
-        # Should have at least a editable text box and a button to make it go
-        # Figure out how to hook into moderngl library
-        # Then give the user string to the library to work magic
-        # Stretch goals:
-        #   * Make the input boxes monospace
-        #   * Add help for possible inputs
         configPath = QStandardPaths.writableLocation(QStandardPaths.GenericConfigLocation)
         self.settings = QSettings(configPath + '/krita-scripterrc', QSettings.IniFormat)
         self.mainDialog = MainDialog(self)
